f2e4587fa5486160def21589f6c8a316d1146dc7
javiercarpio57 <javierrr.carpio57@gmail.com>
2018-06-14 13:15:22 -0600

    Funcionalidades

---
+++ GameCharlie/Assets/Scripts/Character.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Character : MonoBehaviour {
+
+    public Rigidbody2D rgb;
+    public GameObject feet;
+
+    public LayerMask layerMask;
+    Animator anim;
+    private float points = 0f;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        rgb = GetComponent<Rigidbody2D>();
+
+        anim.SetBool("InFloor", true);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        points += Time.deltaTime;
+
+        if (points < 1f)
+        {
+            GameController.instance.score.text = "0";
+        }
+        else
+        {
+            GameController.instance.score.text = "" + (points.ToString("#"));
+        }
+
+        RaycastHit2D raycast = Physics2D.Raycast(feet.transform.position, Vector2.down, 0.1f, layerMask);
+        if (raycast.collider != null)
+        {
+            anim.SetBool("InFloor", true);
+        }
+        else
+        {
+            if (rgb.velocity.y >= -1)
+            {
+                anim.SetBool("InFloor", false);
+            }
+            else if (rgb.velocity.y < -1)
+            {
+                anim.SetBool("InFloor", true);
+            }
+        }
+
+        
+    }
+    
+    public void Fly()
+    {
+        Debug.Log("Holi");
+        rgb.AddForce(Vector2.up * 250f);
+        anim.SetBool("InFloor", false);
+    }
+}
---
+++ GameCharlie/Assets/Scripts/GameController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+    public bool gameOver = false;
+    public static GameController instance;
+    public Text score;
+	// Use this for initialization
+	void Start () {
+        instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
